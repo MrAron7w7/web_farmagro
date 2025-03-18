677dec3a857dae739beceacdde6c026e8aba9ff9
MrAron7w7 <magallanestorresaron@gmail.com>
2025-03-17 22:57:39 -0500

    He agregado la navegacion appbar KazumaGod :3

--- lib/features/views/layout/app_layout.dart
+++ lib/features/views/layout/app_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class AppLayout extends StatefulWidget {
   final Widget? content;
@@ -10,33 +11,51 @@ class AppLayout extends StatefulWidget {
 
 class _AppLayoutState extends State<AppLayout> {
   int _selectedIndex = 0;
+  final Set<int> _hoveredIcons = {};
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Obtener el índice seleccionado desde los parámetros de la ruta
+    final routeArgs = ModalRoute.of(context)?.settings.arguments;
+    if (routeArgs is int) {
+      _selectedIndex = routeArgs;
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-    // Navegar a la página correspondiente
+    // Navegar a la página correspondiente y pasar el índice seleccionado
     switch (index) {
       case 0:
-        Navigator.pushNamed(context, '/home');
+        Navigator.pushReplacementNamed(
+          context,
+          '/home',
+          arguments: index, // Pasar el índice seleccionado
+        );
         break;
       case 1:
-        Navigator.pushNamed(context, '/store');
+        Navigator.pushReplacementNamed(
+          context,
+          '/about',
+          arguments: index, // Pasar el índice seleccionado
+        );
         break;
       case 2:
-        Navigator.pushNamed(context, '/about');
+        Navigator.pushReplacementNamed(
+          context,
+          '/products',
+          arguments: index, // Pasar el índice seleccionado
+        );
         break;
       case 3:
-        Navigator.pushNamed(context, '/activities');
-        break;
-      case 4:
-        Navigator.pushNamed(context, '/products');
-        break;
-      case 5:
-        Navigator.pushNamed(context, '/branches');
-        break;
-      case 6:
-        Navigator.pushNamed(context, '/contact');
+        Navigator.pushReplacementNamed(
+          context,
+          '/contact',
+          arguments: index, // Pasar el índice seleccionado
+        );
         break;
     }
   }
@@ -48,56 +67,63 @@ class _AppLayoutState extends State<AppLayout> {
       body: Column(
         children: [
           // Imagen en la parte superior
-          Center(child: Image.asset('assets/logo.png', height: 100)),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            color: const Color(0xfff5f5f5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                //APPBAR
+                const Text(
+                  '1800 - Farmagro',
+                  style: TextStyle(color: Color(0xffbebebe)),
+                ),
+                Image.asset(
+                  'assets/logo.png',
+                  height: 70,
+                  width: 150,
+                  fit: BoxFit.fill,
+                ),
                 Row(
                   children: [
-                    Text(
-                      "Farmagro",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                      ),
+                    _buildSocialNetwork(
+                      index: 0,
+                      icon: LucideIcons.facebook,
+                      onTap: () => print('Facebook'),
                     ),
-                    SizedBox(width: 45),
+                    _buildSocialNetwork(
+                      index: 1,
+                      icon: LucideIcons.instagram,
+                      onTap: () => print('Instagram'),
+                    ),
+                    _buildSocialNetwork(
+                      index: 2,
+                      icon: LucideIcons.twitter,
+                      onTap: () => print('Twitter'),
+                    ),
+                    _buildSocialNetwork(
+                      index: 3,
+                      icon: LucideIcons.phone,
+                      onTap: () => print('Phone'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // APPBAR
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
                     _buildNavItem("Inicio", 0),
-                    SizedBox(width: 20),
-                    _buildNavItem("Tienda en Línea", 1),
-                    SizedBox(width: 20),
-                    _buildNavItem("Nosotros", 2),
-                    SizedBox(width: 20),
-                    _buildNavItem("Actividades", 3),
-                    SizedBox(width: 20),
-                    _buildNavItem("Catálogo de productos", 4),
-                    SizedBox(width: 20),
-                    _buildNavItem("Sucursales", 5),
-                    SizedBox(width: 20),
-                    _buildNavItem("Contáctenos", 6),
-                    Spacer(),
-                    SizedBox(
-                      width: 250,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Buscar tu producto",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                              color: Color(0XFF2563EB),
-                              width: 3,
-                            ),
-                          ),
-                          prefixIcon: Icon(Icons.search),
-                        ),
-                      ),
-                    ),
+                    _buildNavItem("Nosotros", 1),
+                    _buildNavItem("Catálogo de productos", 2),
+                    _buildNavItem("Contáctenos", 3),
                   ],
                 ),
               ],
@@ -109,18 +135,58 @@ class _AppLayoutState extends State<AppLayout> {
     );
   }
 
+  Widget _buildSocialNetwork({
+    required int index,
+    required IconData icon,
+    required Function()? onTap,
+  }) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hoveredIcons.add(index)),
+      onExit: (_) => setState(() => _hoveredIcons.remove(index)),
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Icon(
+            icon,
+            size: 24,
+            color:
+                _hoveredIcons.contains(index)
+                    ? const Color(0xff45864e)
+                    : Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _buildNavItem(String title, int index) {
-    return InkWell(
-      onTap: () => _onItemTapped(index),
-      child: MouseRegion(
-        onEnter: (event) => setState(() {}),
-        onExit: (event) => setState(() {}),
-        child: Text(
-          title,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            color: _selectedIndex == index ? Colors.green : Colors.grey,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: InkWell(
+        onTap: () => _onItemTapped(index),
+        child: Ink(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color:
+                    _selectedIndex == index ? Colors.green : Colors.transparent,
+                width: 2,
+              ),
+            ),
+          ),
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color:
+                    _selectedIndex == index ? Colors.green : Color(0xff634551),
+              ),
+            ),
           ),
         ),
       ),
